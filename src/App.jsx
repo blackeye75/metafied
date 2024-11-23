9b02a0323276840c496415f669a5c774668ecf66
blackeye75 <priyanshuraj22275@gmail.com>
2024-11-23 18:43:51 +0530

    blur

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import "./App.css";
 import CardConatiner from "./component/CardConatiner";
-import HalfVisibleSpot from "./component/HalfVisibleSpot";
+import HalfVisibleSpot from "./component/IncreasingBlurCircle";
 import Header from "./component/Header";
 
 function App() {
   return (
     <main className="w-full h-full">
       <Header />
+      <HalfVisibleSpot />
       <div className="p-5 pt-2 w-full h-screen ">
         <CardConatiner />
+      </div>
+      <div className="absolute right-48  bottom-40" >
+      <HalfVisibleSpot/>
       </div>
     </main>
   );
---
+++ src/component/IncreasingBlurCircle.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const IncreasingBlurCircle = () => {
+  return (
+    <div className="flex justify-center items-center h-fit w-fit bg-gray-900">
+      {/* Circle with increasing blur */}
+      <div className="relative">
+        <div className="absolute -top-28 -left-14 w-96 h-96 rounded-full bg-gradient-radial from-indigo-500/10 via-pink-600/30 to-transparent blur-xl"></div>
+      </div>
+    </div>
+  );
+};
+
+export default IncreasingBlurCircle;
