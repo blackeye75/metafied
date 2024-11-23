fb61ee7b17b64eecfb92b5eb224defbebacaeff7
blackeye75 <priyanshuraj22275@gmail.com>
2024-11-23 18:55:38 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import Header from "./component/Header";
 
 function App() {
   return (
-    <main className="w-full h-full">
+    <main className="w-full  h-full">
       <Header />
       <HalfVisibleSpot />
       <div className="p-5 pt-2 w-full h-screen ">
         <CardConatiner />
       </div>
-      <div className="absolute right-48  bottom-40" >
+      <div className="absolute  right-60  bottom-40" >
       <HalfVisibleSpot/>
       </div>
     </main>
